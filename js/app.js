62f1a27c5b73b7eac83bf2be280ed6810534eebd
Jony <Jonyyy81@gmail.com>
2021-01-20 02:34:03 +0600

    First Commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+const menuList = document.getElementById("menulist");
+menuList.addEventListener("click",function()  {
+  const SectionHide = document.getElementById("sectionHide");
+  SectionHide.style.display = "none";
+
+  const sultanMenu = document.getElementById("sultanmenu");
+  sultanMenu.style.display = "block";
+
+
+});
+
+
